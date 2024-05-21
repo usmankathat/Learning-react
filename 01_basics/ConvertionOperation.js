b4fe2441d1c76ec93c07ee5649265d7bc0ad8790
SkyLAR <56186130+usmankathat@users.noreply.github.com>
2024-05-21 06:58:50 +0000

    Type Casting

---
+++ 01_basics/ConvertionOperation.js
@@ -0,0 +1,24 @@
+let number = 22
+
+let  numberToString = String(number)
+
+console.log(typeof numberToString)
+let char = "22"
+let charToNumber=Number(char)
+console.log(typeof charToNumber)
+
+let x=1
+let bool=Boolean(x)
+console.log(bool)
+
+let newBool=true
+let newStr = String(newBool)
+console.log(newStr)
+console.log(typeof newStr)
+
+let falseBool= "false"
+let newBoolFalse=Boolean(falseBool)
+console.log(typeof newBoolFalse, newBoolFalse)
+
+let ronik=null
+console.log(typeof null)
