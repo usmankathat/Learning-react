02871fed55fd15f207e6f84acdd42f20f0f6ed70
SkyLAR <56186130+usmankathat@users.noreply.github.com>
2024-05-21 07:21:52 +0000

    Type Castingwith number and string

--- 01_basics/ConvertionOperation.js
+++ 01_basics/ConvertionOperation.js
@@ -13,12 +13,19 @@ console.log(bool)
 
 let newBool=true
 let newStr = String(newBool)
-console.log(newStr)
-console.log(typeof newStr)
+console.log(newStr) //"true"
+console.log(typeof newStr)  //String
 
 let falseBool= "false"
 let newBoolFalse=Boolean(falseBool)
-console.log(typeof newBoolFalse, newBoolFalse)
+console.log(typeof newBoolFalse, newBoolFalse) //true
 
 let ronik=null
-console.log(typeof null)
+console.log(typeof null) //Object
+
+console.log(1+"2")//12
+console.log("1"+2)//12
+console.log(1+2+"3")//33
+
+console.log(+true) //1
+console.log(+"") //0
